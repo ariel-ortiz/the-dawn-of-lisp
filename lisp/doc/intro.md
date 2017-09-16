ecffc0d97797300abcc8153744e7e4f5f49b91bf
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-09-16 15:51:18 +0000

    Initial lisp empty project.

---
+++ lisp/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lisp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ lisp/test/lisp/core_test.clj
@@ -0,0 +1,7 @@
+(ns lisp.core-test
+  (:require [clojure.test :refer :all]
+            [lisp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
