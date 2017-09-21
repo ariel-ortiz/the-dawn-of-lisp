37993c3bddca703f6521e073d7855dbf0407ee14
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-09-20 23:33:38 -0500

    Added $cond macro.

--- lisp/src/lisp/core.clj
+++ lisp/src/lisp/core.clj
@@ -6,9 +6,15 @@
   [& args]
   (println "Hello, World!"))
 
-(declare $apply $atom? $eq? $car $cdr $caar $cadr $cdar
-         $eval $caddr $pairlis $cons $assoc $evcon $evlis
-         $cadar $null?)
+(declare $apply $assoc $atom $car $caar $cadr $cadar
+         $caddr $cdr $cdar $cons $eq $eval $evcon $evlis
+         $null $pairlis)
+
+(defmacro $cond
+  ([[pred exp]]
+   `(if ~pred ~exp))
+  ([[pred exp] & clauses]
+   `(if ~pred ~exp ($cond ~@clauses))))
 
 (defn $evalquote
   [fun x]
@@ -16,69 +22,69 @@
 
 (defn $apply
   [fun x a]
-  (cond
+  ($cond
 
-    ($atom? fun)
-    (cond
-      ($eq? fun 'CAR)  ($caar x)
-      ($eq? fun 'CDR)  ($cdar x)
-      ($eq? fun 'CONS) ($cons ($car x) ($cadr x))
-      ($eq? fun 'ATOM) ($atom? ($car x))
-      ($eq? fun 'EQ)   ($eq? ($car x) ($cadr x))
-      true             ($apply ($eval fun a) x a))
+    (($atom fun)
+     ($cond
+       (($eq fun 'CAR)  ($caar x))
+       (($eq fun 'CDR)  ($cdar x))
+       (($eq fun 'CONS) ($cons ($car x) ($cadr x)))
+       (($eq fun 'ATOM) ($atom ($car x)))
+       (($eq fun 'EQ)   ($eq ($car x) ($cadr x)))
+       (true            ($apply ($eval fun a) x a))))
 
-    ($eq? ($car fun) 'LAMBDA)
-    ($eval ($caddr fun)
-           ($pairlis ($cadr fun) x a))
+    (($eq ($car fun) 'LAMBDA)
+     ($eval ($caddr fun)
+            ($pairlis ($cadr fun) x a)))
 
-    ($eq? ($car fun) 'LABEL)
-    ($apply ($caddr fun)
-            x
-            ($cons ($cons ($cadr fun)
+    (($eq ($car fun) 'LABEL)
+     ($apply ($caddr fun)
+             x
+             ($cons ($cons ($cadr fun)
                           ($caddr fun))
-                   a))))
+                    a)))))
 
 (defn $eval
   [e a]
-  (cond
+  ($cond
 
-    ($atom? e)
-    ($cadr ($assoc e a))
+    (($atom e)
+     ($cadr ($assoc e a)))
 
-    ($atom? ($car e))
-    (cond
-      ($eq? ($car e) 'QUOTE) ($cadr e)
-      ($eq? ($car e) 'COND)  ($evcon ($cdr e) a)
-      true                   ($apply ($car e)
-                                     ($evlis ($cdr e) a)
-                                     a))
-    true
-    ($apply ($car e)
+    (($atom ($car e))
+     ($cond
+       (($eq ($car e) 'QUOTE) ($cadr e))
+       (($eq ($car e) 'COND)  ($evcon ($cdr e) a))
+       (true                   ($apply ($car e)
+                                       ($evlis ($cdr e) a)
+                                       a))))
+    (true
+     ($apply ($car e)
             ($evlis ($cdr e) a)
-            a)))
+            a))))
 
 (defn $evcon
   [c a]
-  (cond
-    ($eval ($caar c) a) ($eval ($cadar c) a)
-    true                ($evcon ($cdr c) a)))
+  ($cond
+    (($eval ($caar c) a) ($eval ($cadar c) a))
+    (true                ($evcon ($cdr c) a))))
 
 (defn $evlis
   [m a]
-  (cond
-    ($null? m) ()
-    true       ($cons ($eval ($car m) a)
-                      ($evlis ($cdr m) a))))
+  ($cond
+    (($null m) ())
+    (true      ($cons ($eval ($car m) a)
+                      ($evlis ($cdr m) a)))))
 
 (defn $pairlis
   "This function gives the list of pairs of corresponding
   elements of the lists x and y, and appends this to the
   list a."
   [x y a]
-  (cond
-    ($null? x) a
-    true       ($cons ($cons ($car x) ($car y))
-                      ($pairlis ($cdr x) ($cdr y) a))))
+  ($cond
+    (($null x) a)
+    (true      ($cons ($cons ($car x) ($car y))
+                      ($pairlis ($cdr x) ($cdr y) a)))))
 
 (defn $assoc
   "If a is an association list such as the one formed by
@@ -86,21 +92,24 @@
   whose first term is x. Thus it is a table searching
   function."
   [x a]
-  (cond
-    ($eq? ($caar a) x) ($car a) ; Lisp 1.5 manual uses equal
-    true               ($assoc x ($cdr a))))
+  ($cond
+    (($eq ($caar a) x) ($car a)) ; Lisp 1.5 manual uses equal instead of eq.
+    (true              ($assoc x ($cdr a)))))
 
 (defn $cons
   [a b]
-  (if (seq? b)
-    (cons a b)
-    (list a b)))
+  ($cond
+    ((seq? b) (cons a b))
+    (true     (list a b))))
 
-(defn $atom?
+(defn $atom
   [x]
-  (or (symbol? x) (empty? x)))
+  ($cond
+    ((symbol? x)               true)
+    ((and (seq? x) (empty? x)) true)
+    (true                      false)))
 
-(def $eq? =)
+(def $eq =)
 
 (def $car first)
 
@@ -116,4 +125,5 @@
 
 (def $cadar (comp $car $cdr $car))
 
-(def $null? empty?)
+(def $null empty?)
+
