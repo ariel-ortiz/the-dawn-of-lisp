44fc64f2138f7f0889b7bbd1ad39ba74977e356c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-09-21 23:20:01 -0500

    Completed documentation and added more tests.

--- lisp-one-point-five/src/lisp_one_point_five/core.clj
+++ lisp-one-point-five/src/lisp_one_point_five/core.clj
@@ -1,94 +1,42 @@
 (ns lisp-one-point-five.core)
 
-(declare $apply $assoc $atom $car $caar $cadr $cadar
-         $caddr $cdr $cdar $cons $eq $eval $evcon $evlis
-         $null $pairlis)
+(declare $apply $eval $evcon $evlis)
+
+(def $T
+  "Synonym for true."
+  true)
+
+(def $F
+  "Synonym for false."
+  false)
+
+(def $NIL
+  "Synonym for the empty (null) list."
+  ())
 
 (defmacro $cond
+  "Conditional expression of the form:
+
+     ($cond
+       (p1 e1)
+       (p2 e2)
+       ...
+       (pn en))
+
+  where each pi is an expression whose value may be truth
+  or falsity, and each ei is any expression. The meaning
+  of a conditional expression is: if p1 is true, then the
+  value of e1 is the value of the entire expression. If
+  p1 is false, then if p2 is true the value of e2 is
+  the value of the entire expression. The pi are searched
+  from left to right until the first true one is found.
+  Then the corresponding ei is selected. If none of the
+  pi are true, then the value of the entire expression
+  is undefined."
   ([[pred exp]]
    `(if ~pred ~exp))
   ([[pred exp] & clauses]
    `(if ~pred ~exp ($cond ~@clauses))))
-
-(defn $evalquote
-  [fun x]
-  ($apply fun x ()))
-
-(defn $apply
-  [fun x a]
-  ($cond
-
-    (($atom fun)
-     ($cond
-       (($eq fun 'CAR)  ($caar x))
-       (($eq fun 'CDR)  ($cdar x))
-       (($eq fun 'CONS) ($cons ($car x) ($cadr x)))
-       (($eq fun 'ATOM) ($atom ($car x)))
-       (($eq fun 'EQ)   ($eq ($car x) ($cadr x)))
-       (true            ($apply ($eval fun a) x a))))
-
-    (($eq ($car fun) 'LAMBDA)
-     ($eval ($caddr fun)
-            ($pairlis ($cadr fun) x a)))
-
-    (($eq ($car fun) 'LABEL)
-     ($apply ($caddr fun)
-             x
-             ($cons ($cons ($cadr fun)
-                           ($caddr fun))
-                    a)))))
-
-(defn $eval
-  [e a]
-  ($cond
-
-    (($atom e)
-     ($cadr ($assoc e a)))
-
-    (($atom ($car e))
-     ($cond
-       (($eq ($car e) 'QUOTE) ($cadr e))
-       (($eq ($car e) 'COND)  ($evcon ($cdr e) a))
-       (true                  ($apply ($car e)
-                                      ($evlis ($cdr e) a)
-                                      a))))
-    (true
-     ($apply ($car e)
-             ($evlis ($cdr e) a)
-             a))))
-
-(defn $evcon
-  [c a]
-  ($cond
-    (($eval ($caar c) a) ($eval ($cadar c) a))
-    (true                ($evcon ($cdr c) a))))
-
-(defn $evlis
-  [m a]
-  ($cond
-    (($null m) ())
-    (true      ($cons ($eval ($car m) a)
-                      ($evlis ($cdr m) a)))))
-
-(defn $pairlis
-  "This function gives the list of pairs of corresponding
-  elements of the lists x and y, and appends this to the
-  list a."
-  [x y a]
-  ($cond
-    (($null x) a)
-    (true      ($cons ($cons ($car x) ($car y))
-                      ($pairlis ($cdr x) ($cdr y) a)))))
-
-(defn $assoc
-  "If a is an association list such as the one formed by
-  $pairlis, then $assoc will produce the first pair
-  whose first term is x. Thus it is a table searching
-  function."
-  [x a]
-  ($cond
-    (($eq ($caar a) x) ($car a)) ; Lisp 1.5 manual uses equal instead of eq.
-    (true              ($assoc x ($cdr a)))))
 
 (defn $cons
   "Obtains a new word from the free storage list and places
@@ -97,7 +45,7 @@
   [a b]
   ($cond
     ((seq? b) (cons a b))
-    (true     (list a b))))
+    ($T       (list a b))))
 
 (defn $atom
   "Returns true if its argument is an atomic symbol, and
@@ -105,9 +53,9 @@
   considered atomic."
   [x]
   ($cond
-    ((symbol? x)               true)
-    ((and (seq? x) (empty? x)) true)
-    (true                      false)))
+    ((symbol? x)               $T)
+    ((and (seq? x) (empty? x)) $T)
+    ($T                        $F)))
 
 (defn $eq
   "Returns true if both arguments are the same atomic
@@ -124,8 +72,8 @@
   first)
 
 (def $cdr
-  "Returns the second part of its composite argument. The
-  cdr of an atomic symbol is undefined."
+  "Returns the second part (rest) of its composite
+  argument. The cdr of an atomic symbol is undefined."
   rest)
 
 (def $caar
@@ -152,3 +100,101 @@
   "Returns true if its argument is an empty list, or false
   otherwise."
   empty?)
+
+(defn $pairlis
+  "This function gives the list of pairs of corresponding
+  elements of the lists x and y, and appends this to the
+  list a."
+  [x y a]
+  ($cond
+    (($null x) a)
+    ($T        ($cons ($cons ($car x) ($car y))
+                      ($pairlis ($cdr x) ($cdr y) a)))))
+
+(defn $assoc
+  "If a is an association list such as the one formed by
+  $pairlis, then $assoc will produce the first pair
+  whose first term is x. Thus it is a table searching
+  function."
+  [x a]
+  ($cond
+    (($eq ($caar a) x) ($car a)) ; Lisp 1.5 manual uses equal instead of eq.
+    ($T                ($assoc x ($cdr a)))))
+
+(defn $evalquote
+  "A universal Lisp function. When evalquote is given a
+  function fun and a list of arguments x for that
+  function, it computes the value of the function applied
+  to the arguments."
+  [fun x]
+  ($apply fun x $NIL))
+
+(defn $apply
+  "Handles a function fun and its arguments in the list x.
+  The argument a is used as an association list for
+  storing the values of bound variables and function
+  names."
+  [fun x a]
+  ($cond
+
+    (($atom fun)
+     ($cond
+       (($eq fun 'CAR)  ($caar x))
+       (($eq fun 'CDR)  ($cdar x))
+       (($eq fun 'CONS) ($cons ($car x) ($cadr x)))
+       (($eq fun 'ATOM) ($atom ($car x)))
+       (($eq fun 'EQ)   ($eq ($car x) ($cadr x)))
+       ($T              ($apply ($eval fun a) x a))))
+
+    (($eq ($car fun) 'LAMBDA)
+     ($eval ($caddr fun)
+            ($pairlis ($cadr fun) x a)))
+
+    (($eq ($car fun) 'LABEL)
+     ($apply ($caddr fun)
+             x
+             ($cons ($cons ($cadr fun)
+                           ($caddr fun))
+                    a)))))
+
+(defn $eval
+  "Handles the forms in e. The argument a is used as an
+  association list for storing the values of bound
+  variables and function names."
+  [e a]
+  ($cond
+
+    (($atom e)
+     ($cadr ($assoc e a)))
+
+    (($atom ($car e))
+     ($cond
+       (($eq ($car e) 'QUOTE) ($cadr e))
+       (($eq ($car e) 'COND)  ($evcon ($cdr e) a))
+       ($T                    ($apply ($car e)
+                                      ($evlis ($cdr e) a)
+                                      a))))
+    ($T
+     ($apply ($car e)
+             ($evlis ($cdr e) a)
+             a))))
+
+(defn $evcon
+  "Evaluates the propositional terms in order, and chooses
+  the form following the first true predicate. The argument
+  a is used as an association list for storing the values
+  of bound variables and function names."
+  [c a]
+  ($cond
+    (($eval ($caar c) a) ($eval ($cadar c) a))
+    ($T                  ($evcon ($cdr c) a))))
+
+(defn $evlis
+  "Evaluate all expressions in list m. The argument a is
+  used as an association list for storing the values of
+  bound  variables and function names."
+  [m a]
+  ($cond
+    (($null m) $NIL)
+    ($T        ($cons ($eval ($car m) a)
+                      ($evlis ($cdr m) a)))))
--- lisp-one-point-five/test/lisp_one_point_five/core_test.clj
+++ lisp-one-point-five/test/lisp_one_point_five/core_test.clj
@@ -3,29 +3,29 @@
             [lisp-one-point-five.core :refer :all]))
 
 (deftest test-$cons
-  (is (= '(X) ($cons 'X ())))
+  (is (= '(X) ($cons 'X $NIL)))
   (is (= '(X A B C) ($cons 'X '(A B C))))
   (is (= '((A B C) X Y Z) ($cons '(A B C) '(X Y Z))))
   (is (= '(A B) ($cons 'A 'B))))
 
 (deftest test-$atom
-  (is ($atom 'X))
-  (is ($atom ()))
-  (is (not ($atom '(X))))
-  (is (not ($atom '(X Y Z)))))
+  (is (= $T ($atom 'X)))
+  (is (= $T ($atom $NIL)))
+  (is (= $F ($atom '(X))))
+  (is (= $F ($atom '(X Y Z)))))
 
 (deftest test-$eq
-  (is ($eq 'ALPHA 'ALPHA))
-  (is ($eq () ()))
-  (is (not ($eq 'ALPHA 'BETA)))
-  (is (not ($eq () 'ALPHA))))
+  (is (= $T ($eq 'ALPHA 'ALPHA)))
+  (is (= $T ($eq $NIL $NIL)))
+  (is (= $F ($eq 'ALPHA 'BETA)))
+  (is (= $F ($eq $NIL 'ALPHA))))
 
 (deftest test-$car
   (is (= 'A ($car '(A))))
   (is (= 'A ($car '(A B C D E)))))
 
 (deftest test-$cdr
-  (is (= () ($cdr '(A))))
+  (is (= $NIL ($cdr '(A))))
   (is (= '(B C D E) ($cdr '(A B C D E)))))
 
 (deftest test-$cxxxr
@@ -36,5 +36,19 @@
   (is (= 'B   ($cadar '((A B) C D E)))))
 
 (deftest test-$null
-  (is ($null ()))
-  (is (not ($null '(A B C)))))
+  (is (= $T ($null $NIL)))
+  (is (= $F ($null '(A B C)))))
+
+(deftest test-$pairlis
+  (is (= $NIL
+         ($pairlis $NIL $NIL $NIL)))
+  (is (= '((A D) (B E) (C F))
+         ($pairlis '(A B C) '(D E F) $NIL)))
+  (is (= '((A B) (C D) (E F))
+         ($pairlis $NIL $NIL '((A B) (C D) (E F)))))
+  (is (= '((A D) (B E) (C F) (W X) (Y Z))
+         ($pairlis '(A B C) '(D E F) '((W X) (Y Z))))))
+
+(deftest test-$assoc
+  (is (= '(A B) ($assoc 'A '((A B)))))
+  (is (= '(E F) ($assoc 'E '((A B) (C D) (E F) (G H))))))
