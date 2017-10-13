5fb1ae074a5b43fc4042fea2ef53c720a248e6af
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-10-13 11:14:55 -0400

    Added copyright and licence header in a comment.

--- lisp-one-point-five/src/lisp_one_point_five/core.clj
+++ lisp-one-point-five/src/lisp_one_point_five/core.clj
@@ -1,3 +1,23 @@
+;;; Implementation of Lisp 1.5 in Clojure, as formally
+;;; defined in p. 13 of "LISP 1.5 Programmer's Manual"
+;;; by John McCarthy.
+;;;
+;;; Copyright (C) 2017 by Ariel Ortiz,
+;;; Tecnologico de Monterrey, CEM.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns lisp-one-point-five.core)
 
 (declare $apply $eval $evcon $evlis)
