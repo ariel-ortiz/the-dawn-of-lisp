751552a19b085d8e9478370e7c76f75f3415d979
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-03-28 17:36:23 +0000

    Replace in ns ":refer :all" with ":refer [...]"

--- lisp-one-point-five/test/lisp_one_point_five/core_test.clj
+++ lisp-one-point-five/test/lisp_one_point_five/core_test.clj
@@ -1,6 +1,9 @@
 (ns lisp-one-point-five.core-test
-  (:require [clojure.test :refer :all]
-            [lisp-one-point-five.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [lisp-one-point-five.core :refer [$F $NIL $T $apply $assoc $atom
+                                              $caar $cadar $caddr $cadr $car
+                                              $cdar $cdr $cons $eq $eval
+                                              $evalquote $null $pairlis]]))
 
 (deftest test-$cons
   (is (= '(X) ($cons 'X $NIL)))
@@ -84,30 +87,30 @@
          ($apply 'MAPCAR
                  '(CDR ((A B C) (D E F) (F G H) (I J K)))
                  '((MAPCAR (LAMBDA (F X)
-                             (COND
-                               ((EQ X NIL) NIL)
-                               (T          (CONS (F (CAR X))
-                                                 (MAPCAR F (CDR X)))))))
+                                   (COND
+                                    ((EQ X NIL) NIL)
+                                    (T          (CONS (F (CAR X))
+                                                      (MAPCAR F (CDR X)))))))
                    (NIL ())
                    (T   true)))))
   (is (= $NIL
          ($apply '(LABEL DUPALL
                          (LAMBDA (X)
-                           (COND
-                             ((EQ X NIL) NIL)
-                             (T          (CONS (CAR X)
-                                               (CONS (CAR X)
-                                                     (DUPALL (CDR X))))))))
+                                 (COND
+                                  ((EQ X NIL) NIL)
+                                  (T          (CONS (CAR X)
+                                                    (CONS (CAR X)
+                                                          (DUPALL (CDR X))))))))
                  '(())
                  '((NIL ()) (T true)))))
   (is (= '(A A B B C C D D)
          ($apply '(LABEL DUPALL
                          (LAMBDA (X)
-                           (COND
-                             ((EQ X NIL) NIL)
-                             (T          (CONS (CAR X)
-                                               (CONS (CAR X)
-                                                     (DUPALL (CDR X))))))))
+                                 (COND
+                                  ((EQ X NIL) NIL)
+                                  (T          (CONS (CAR X)
+                                                    (CONS (CAR X)
+                                                          (DUPALL (CDR X))))))))
                  '((A B C D))
                  '((NIL ()) (T true))))))
 
@@ -144,10 +147,10 @@
   (is (= '((A A) (B B) (C C))
          ($eval '((LABEL MAPCAR
                          (LAMBDA (FN LST)
-                           (COND
-                             ((EQ NIL LST) NIL)
-                             (T (CONS (FN (CAR LST))
-                                      (MAPCAR FN (CDR LST)))))))
+                                 (COND
+                                  ((EQ NIL LST) NIL)
+                                  (T (CONS (FN (CAR LST))
+                                           (MAPCAR FN (CDR LST)))))))
                   DUP
                   LST)
                 '((NIL ())
@@ -158,7 +161,7 @@
   ; variables with dynamic scoping.
   (is (= '((ALPHA OMEGA) BETA GAMMA)
          ($eval '((LAMBDA (X)
-                    ((LAMBDA (Y) (F Y)) B))
+                          ((LAMBDA (Y) (F Y)) B))
                   (F A))
                 '((F (LAMBDA (Z) (CONS X Z)))
                   (A (OMEGA))
